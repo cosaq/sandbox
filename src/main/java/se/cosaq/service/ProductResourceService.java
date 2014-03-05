52f6d57d4e83f566a341cb9ebf31d7fa2b3e6aad
Anders Eriksson <anders.eriksson@cosaq.se>
2014-03-05 22:18:57 +0100

    entered TO-DO comment

--- src/main/java/se/cosaq/service/ProductResourceService.java
+++ src/main/java/se/cosaq/service/ProductResourceService.java
@@ -1,12 +1,10 @@
 package se.cosaq.service;
 
-import java.io.InputStream;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 @Path("/Product")
 public class ProductResourceService implements ProductResource {
@@ -14,7 +12,8 @@ public class ProductResourceService implements ProductResource {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getIt() {
-		return "Got PRODUCT!";
+		//TO-DO
+		return "Got PRODUCT! ";
 	}
 
 }
