9d3fe688cf495c350a1a49d4823aa03ce282425a
Anders Eriksson <anders.eriksson@cosaq.se>
2014-03-05 22:24:22 +0100

    tagit bort annotations från imp.klass

--- src/main/java/se/cosaq/service/ProductResourceService.java
+++ src/main/java/se/cosaq/service/ProductResourceService.java
@@ -1,19 +1,14 @@
 package se.cosaq.service;
 
 
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 @Path("/Product")
 public class ProductResourceService implements ProductResource {
 
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
 	public String getIt() {
 		//TO-DO
-		return "Got PRODUCT! ";
+		return "Got PRODUCT! Nu utan annotations. Nästan";
 	}
 
 }
